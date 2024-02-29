72921762dbb8668e07d123d8128fdbfa446dd10f
SergiyGenza <63913766+SergiyGenza@users.noreply.github.com>
2024-02-29 15:12:35 +0200

    refactor: working on the unification of elements to create a universal template

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardComponent {
-
+  @Input() projectPreview: string | undefined;
+  @Input() title: string | undefined;
+  @Input() text: string | undefined;
+  @Input() stack: string | undefined;
+  @Input() previewUrl: string | undefined;
+  @Input() codewUrl: string | undefined;
 }
--- src/app/shared/experiance-card/experiance-card.component.ts
+++ src/app/shared/experiance-card/experiance-card.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-experiance-card',
   templateUrl: './experiance-card.component.html',
-  styleUrls: ['./experiance-card.component.scss']
+  styleUrls: ['./experiance-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExperianceCardComponent {
   @Input() content: any;
